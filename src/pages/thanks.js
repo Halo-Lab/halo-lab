d70a5fbb89e2ec0dbe03bedce1330a7688015267
mikhailnetkachev-halolab <michael.n@halo-lab.com>
2020-01-11 17:05:46 +0200

    Added: Thanks Page.

---
+++ src/pages/thanks.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import ApplicationLayout from "@components/ApplicationLayout";
+import Head from "@components/Head";
+
+const Thanks = () => {
+
+  return (
+    <ApplicationLayout>
+      <Head>
+        <title>Thanks - Halo Lab Blog</title>
+      </Head>
+      <div>Thanks</div>
+    </ApplicationLayout>
+  );
+};
+
+export default Thanks;
