93dc39fbf5e9a9d3a8593ce9b222f4a4dbd3be5d
mikhailnetkachev-halolab <michael.n@halo-lab.com>
2020-01-22 16:51:43 +0200

    --amend

--- src/pages/test.js
+++ src/pages/test.js
@@ -1,18 +1,9 @@
 import React from 'react';
 
-import ModalWrapper from '@components/ModalWrapper';
-
 const Test = () => {
   return (
     <div>
       <div>Test</div>
-      <ModalWrapper
-        button={{
-          title: 'Click me, click',
-        }}
-      >
-        <div>Modal content is Here</div>
-      </ModalWrapper>
     </div>
   );
 };
