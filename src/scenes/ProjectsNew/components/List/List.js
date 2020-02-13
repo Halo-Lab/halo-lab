638e70ce514fb7169f89b84e1ba1bbdb63026056
mikhailnetkachev-halolab <michael.n@halo-lab.com>
2020-02-13 12:13:15 +0200

    Created ProjectScene component in Project/List scene.

--- src/scenes/ProjectsNew/components/List/List.js
+++ src/scenes/ProjectsNew/components/List/List.js
@@ -1,10 +1,10 @@
 import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
-import Img from 'gatsby-image';
 
 import { GlobalContext } from '@contexts';
 import { useProjectImages } from '@hooks/queries';
 import Partners from './components/Partners';
+import ProjectScene from './components/ProjectScene';
 
 import styles from './List.module.scss';
 
@@ -22,6 +22,7 @@ const List = () => {
 
   const items = [
     {
+      id: 0,
       link: '#',
       preview: preview1,
       tags: 'UX, UI, Illustrations, Icons',
@@ -38,6 +39,7 @@ const List = () => {
       ],
     },
     {
+      id: 1,
       link: '#',
       preview: preview2,
       tags: 'Analytics, UX, UI, Icons, Front-end',
@@ -55,6 +57,7 @@ const List = () => {
       ],
     },
     {
+      id: 2,
       link: '#',
       preview: preview3,
       tags: 'Analytics, UX, UI, Front-end',
@@ -75,36 +78,13 @@ const List = () => {
 
   return (
     <ul className={styles.container}>
-      {items.map(({ link, partners, preview, review, tags, title }, index) => {
+      {items.map((item, index) => {
         const isReversed = !((index + 1) % 2);
+        const { id, partners, ...sceneProperties } = item;
 
         return (
-          <li key={title} className={styles.item}>
-            <div className={styles.project}>
-              <div className={styles.preview}>
-                <a href={link}>
-                  <Img fluid={preview.childImageSharp.fluid} />
-                </a>
-              </div>
-              <div className={styles.description}>
-                <div className={styles.tags}>{tags}</div>
-                <div className={styles.title}>
-                  <a href={link}>{title}</a>
-                </div>
-                <div className={styles.descriptionLink}>
-                  <a href={link}>More info</a>
-                </div>
-                <div className={styles.review}>
-                  <div className={styles.avatar}>
-                    <Img fluid={review.avatar.childImageSharp.fluid} />
-                  </div>
-                  <div className={styles.reviewContent}>
-                    <div className={styles.reviewText}>{review.text}</div>
-                    <div className={styles.reviewAuthor}>{review.author}</div>
-                  </div>
-                </div>
-              </div>
-            </div>
+          <li key={id} className={styles.item}>
+            <ProjectScene {...sceneProperties} reversed={isReversed} />
             <Partners items={partners} reversed={isReversed} />
           </li>
         );
---
+++ src/scenes/ProjectsNew/components/List/components/ProjectScene/ProjectScene.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import Img from 'gatsby-image';
+import PropTypes from 'prop-types';
+
+import styles from './ProjectScene.module.scss';
+
+const ProjectScene = ({ link, preview, reversed, review, tags, title }) => {
+  return (
+    <div className={`${styles.container} ${reversed ? styles.reversed : ''}`}>
+      <div className={styles.preview}>
+        <a href={link}>
+          <Img fluid={preview.childImageSharp.fluid} />
+        </a>
+      </div>
+      <div className={styles.description}>
+        <div className={styles.tags}>{tags}</div>
+        <div className={styles.title}>
+          <a href={link}>{title}</a>
+        </div>
+        <div className={styles.descriptionLink}>
+          <a href={link}>More info</a>
+        </div>
+        <div className={styles.review}>
+          <div className={styles.avatar}>
+            <Img fluid={review.avatar.childImageSharp.fluid} />
+          </div>
+          <div className={styles.reviewContent}>
+            <div className={styles.reviewText}>{review.text}</div>
+            <div className={styles.reviewAuthor}>{review.author}</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+ProjectScene.propTypes = {
+  link: PropTypes.string,
+  preview: PropTypes.string,
+  reversed: PropTypes.bool,
+  review: PropTypes.string,
+  tags: PropTypes.string,
+  title: PropTypes.string,
+};
+
+export default ProjectScene;
