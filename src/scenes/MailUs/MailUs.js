48a92fe277b409c049c4bf9342d3952470301103
mikhailnetkachev-halolab <michael.n@halo-lab.com>
2020-02-19 14:06:57 +0200

    Edited MailUs scene: incapsulated styles.

--- src/scenes/MailUs/MailUs.js
+++ src/scenes/MailUs/MailUs.js
@@ -2,15 +2,15 @@ import React from 'react';
 
 import { useSiteMetadata } from '@hooks/queries';
 
-import './MailUs.scss';
+import styles from './MailUs.module.scss';
 
 const MailUs = () => {
   const metadata = useSiteMetadata();
 
   return (
-    <div className="mail_us tac">
-      <p className="mail_us-title">Time to create your star</p>
-      <a href={`mailto:${metadata.email}`} className="mail_us-link">
+    <div className={styles.container}>
+      <p className={styles.title}>Time to create your star</p>
+      <a href={`mailto:${metadata.email}`} className={styles.link}>
         {metadata.email}
       </a>
     </div>
