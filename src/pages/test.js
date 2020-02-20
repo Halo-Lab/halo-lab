bf871d5e1b59ebd6bacfab6e71106bb9e59ac7b1
mikhailnetkachev-halolab <michael.n@halo-lab.com>
2020-02-20 13:54:01 +0200

    Cleaned Test page.

--- src/pages/test.js
+++ src/pages/test.js
@@ -1,26 +1,11 @@
 import React from 'react';
 
 import Providers from '@components/Providers';
-import Ticker from '@components/Ticker';
 
 const Test = () => {
-  const items = [
-    { id: '0', title: 'First' },
-    { id: '1', title: 'Second' },
-    { id: '2', title: 'Third' },
-    { id: '3', title: 'Fourth' },
-    { id: '4', title: 'Fifth' },
-    { id: '5', title: 'Sixth' },
-    { id: '6', title: 'Seventh' },
-    { id: '7', title: 'Eighth' },
-  ];
-
   return (
     <Providers>
-      <div>
-        <div>Test</div>
-        <Ticker images={items} />
-      </div>
+      <div>Test</div>
     </Providers>
   );
 };
