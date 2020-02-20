2ac9bf3fcb5155ecea6d6723c60430352e280147
mikhailnetkachev-halolab <michael.n@halo-lab.com>
2020-02-20 13:43:09 +0200

    Edited Home/Gallery scene: added useBreakpoint hook.

--- src/scenes/HomePage/components/Gallery/Gallery.js
+++ src/scenes/HomePage/components/Gallery/Gallery.js
@@ -1,80 +1,51 @@
-import React, { useContext } from 'react';
+import React from 'react';
 
+import { useGetBreakpoint } from '@hooks';
 import { useGalleryImages } from '@hooks/queries';
 import Slider from '@components/Slider';
-import Item from './components/Item';
-import { GlobalContext } from '@contexts';
 import Ticker from '@components/Ticker';
+import Item from './components/Item';
 
 import styles from './Gallery.module.scss';
 
 const Gallery = () => {
+  const { breakpoint } = useGetBreakpoint();
   const images = useGalleryImages();
-  console.log(images);
 
-  // const images = imagesAPI.getArray([
-  //   'gallery/gallery-1.jpg',
-  //   'gallery/gallery-2.jpg',
-  //   'gallery/gallery-3.jpg',
-  // ]);
-  // const items = [...images, ...images, ...images];
-
-  // const settings = {
-  //   arrows: false,
-  //   slidesToShow: 3,
-  //   slidesToScroll: 1,
-  //   autoplay: true,
-  //   autoplaySpeed: 0,
-  //   speed: 2500,
-  //   cssEase: 'linear',
-  //   responsive: [
-  //     {
-  //       breakpoint: 1024,
-  //       settings: {
-  //         autoplay: true,
-  //         slidesToShow: 3,
-  //       },
-  //     },
-  //     {
-  //       breakpoint: 991,
-  //       settings: {
-  //         autoplay: false,
-  //         speed: 1500,
-  //         slidesToShow: 2,
-  //       },
-  //     },
-  //     {
-  //       breakpoint: 575,
-  //       settings: {
-  //         autoplay: false,
-  //         speed: 1500,
-  //         slidesToShow: 1,
-  //       },
-  //     },
-  //   ],
-  // };
-
-  const myImages = [
-    { id: '0', title: 'First' },
-    { id: '1', title: 'Second' },
-    { id: '2', title: 'Third' },
-    { id: '3', title: 'Fourth' },
-    { id: '4', title: 'Fifth' },
-    { id: '5', title: 'Sixth' },
-    { id: '6', title: 'Seventh' },
-    { id: '7', title: 'Eighth' },
-  ];
+  const settings = {
+    arrows: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 4,
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+    ],
+  };
 
   return (
     <section className={styles.container}>
       <h2 className={styles.title}>We are live</h2>
       <div className={styles.sliderWrapper}>
-        {/* <Slider settings={settings}>
-          {items.map(item => {
-            return <Item key={item.path} {...item} />;
-          })}
-        </Slider> */}
-        <Ticker images={images} />
+        {breakpoint === 'desktop' ? (
+          <Ticker images={images} />
+        ) : (
+          <Slider settings={settings}>
+            {images.map(item => {
+              return <Item key={item.name} {...item} />;
+            })}
+          </Slider>
+        )}
       </div>
     </section>
   );
