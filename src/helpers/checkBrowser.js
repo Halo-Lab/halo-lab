e800886d05bd60d1f0b83630db3e963c55c888ad
mikhailnetkachev-halolab <michael.n@halo-lab.com>
2020-02-25 12:34:58 +0200

    Edited checkBrowser helper: fixed problem with global variable.

--- src/helpers/checkBrowser.js
+++ src/helpers/checkBrowser.js
@@ -1,13 +1,17 @@
 const checkBrowser = () => {
-  const isSafari =
-    /constructor/i.test(window.HTMLElement) ||
-    (function(p) {
-      return p.toString() === '[object SafariRemoteNotification]';
-    })(
-      !window['safari'] ||
-        (typeof window['safari'] !== 'undefined' &&
-          window['safari'].pushNotification)
-    );
+  let isSafari = null;
+
+  if (typeof window !== `undefined`) {
+    isSafari =
+      /constructor/i.test(window.HTMLElement) ||
+      (function(p) {
+        return p.toString() === '[object SafariRemoteNotification]';
+      })(
+        !window['safari'] ||
+          (typeof window['safari'] !== 'undefined' &&
+            window['safari'].pushNotification)
+      );
+  }
 
   return { isSafari };
 };
