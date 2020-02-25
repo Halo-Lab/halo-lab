3c58994314b529f8c8653dcc87e99129afd8eb9a
mikhailnetkachev-halolab <michael.n@halo-lab.com>
2020-02-25 15:15:40 +0200

    Edited Home/Testimonials/Item: created new layout and scoped styles. Added new assets to images. Created useTestimonialsAssets hook.

--- src/scenes/HomePage/components/Testimonials/Testimonials.js
+++ src/scenes/HomePage/components/Testimonials/Testimonials.js
@@ -1,64 +1,52 @@
-import React, { useContext } from 'react';
+import React from 'react';
 
 import Slider from '@components/Slider';
 import Item from './components/Item';
-import { GlobalContext } from '@contexts';
+import { useTestimonialsAssets } from '@hooks/queries';
 
 import styles from './Testimonials.module.scss';
 
 const Testimonials = () => {
-  const { imagesAPI } = useContext(GlobalContext);
-  const images = imagesAPI.get([
-    'stars-5.svg',
-    'clutch-logo.svg',
-    'rating-logo.svg',
-  ]);
+  const {
+    clutchLogo,
+    companyLogo1,
+    companyLogo2,
+    companyLogo3,
+    companyLogo4,
+    companyLogo5,
+    stars,
+  } = useTestimonialsAssets();
 
   const items = [
     {
       author: `Alexander Kozma Ingal, Room Six`,
-      companyLogo: images['rating-logo.svg'],
+      companyLogo: companyLogo1,
       rating: '5.0',
       text: `They perfectly met my expectations — working with them felt like an extension of my in-house team.`,
-      // remove -->
-      stars: images['stars-5.svg'],
-      clutch: images['clutch-logo.svg'],
     },
     {
       author: `Marshall Haas, CEO & Co-Founder, Need/Want`,
-      companyLogo: images['rating-logo.svg'],
+      companyLogo: companyLogo2,
       rating: '5.0',
       text: `The value we get from them and their quality work set them apart from others.`,
-      // remove -->
-      stars: images['stars-5.svg'],
-      clutch: images['clutch-logo.svg'],
     },
     {
       author: `Bogdan Suchyk, Founder Mobalytics`,
-      companyLogo: images['rating-logo.svg'],
+      companyLogo: companyLogo3,
       rating: '5.0',
       text: `I was most appreciative of their ability to provide us with designers for specific needs.`,
-      // remove -->
-      stars: images['stars-5.svg'],
-      clutch: images['clutch-logo.svg'],
     },
     {
       author: `Anton Glance, Founder, Glance Tech Inc.`,
-      companyLogo: images['rating-logo.svg'],
+      companyLogo: companyLogo4,
       rating: '5.0',
       text: `Halo Lab was transparent about deliverables and any challenges the team was facing.`,
-      // remove -->
-      stars: images['stars-5.svg'],
-      clutch: images['clutch-logo.svg'],
     },
     {
       author: `Panos Rigopoulos Cofounder, Software Company`,
-      companyLogo: images['rating-logo.svg'],
+      companyLogo: companyLogo5,
       rating: '5.0',
       text: `Halo Lab works hard to produce good results at a reasonable price.`,
-      // remove -->
-      stars: images['stars-5.svg'],
-      clutch: images['clutch-logo.svg'],
     },
   ];
 
@@ -74,7 +62,14 @@ const Testimonials = () => {
     <section className={styles.container}>
       <Slider settings={settings}>
         {items.map(item => {
-          return <Item key={item.author} {...item} />;
+          return (
+            <Item
+              key={item.author}
+              generalLogo={clutchLogo}
+              stars={stars}
+              {...item}
+            />
+          );
         })}
       </Slider>
     </section>
--- src/scenes/HomePage/components/Testimonials/components/Item/Item.js
+++ src/scenes/HomePage/components/Testimonials/components/Item/Item.js
@@ -2,39 +2,56 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import './Item.scss';
+import styles from './Item.module.scss';
 
-const Item = ({ author, companyLogo, rating, text, stars, clutch }) => {
+const Item = ({ author, companyLogo, generalLogo, rating, stars, text }) => {
   return (
-    <div className="testimonials__item">
-      <div className="testimonials__item-body">
-        <div className="testimonials__item-header">
+    <div className={styles.container}>
+      <div className={styles.review}>
+        <div className={styles.rating}>
           <span>{rating}</span>
-          <img src={stars.url} alt={stars.name} />
+          <img src={stars.publicURL} />
         </div>
-        <div className="testimonials__item-text">&#34;{text}&#34;</div>
-        <div className="testimonials__item-author">— {author}</div>
+        <div className={styles.text}>&#34;{text}&#34;</div>
       </div>
-      <a href="#" className="testimonials__item-footer">
-        <div className="testimonials__item-author">
-          <img src={clutch.url} alt={clutch.name} />
-          <span>— {author}</span>
-        </div>
-        <div className="testimonials__item-logo">
-          <img src={companyLogo.url} alt={companyLogo.name} />
-        </div>
-      </a>
+      <div className={styles.author}>
+        <img src={generalLogo.publicURL} className={styles.generalLogo} />
+        <span className={styles.name}>- {author}</span>
+        <img src={companyLogo.publicURL} className={styles.companyLogo} />
+      </div>
     </div>
   );
+
+  // return (
+  //   <div className="testimonials__item">
+  //     <div className="testimonials__item-body">
+  //       <div className="testimonials__item-header">
+  //         <span>{rating}</span>
+  //         <img src={stars.url} alt={stars.name} />
+  //       </div>
+  //       <div className="testimonials__item-text">&#34;{text}&#34;</div>
+  //       <div className="testimonials__item-author">— {author}</div>
+  //     </div>
+  //     <a href="#" className="testimonials__item-footer">
+  //       <div className="testimonials__item-author">
+  //         <img src={clutch.url} alt={clutch.name} />
+  //         <span>— {author}</span>
+  //       </div>
+  //       <div className="testimonials__item-logo">
+  //         <img src={companyLogo.url} alt={companyLogo.name} />
+  //       </div>
+  //     </a>
+  //   </div>
+  // );
 };
 
 Item.propTypes = {
   author: PropTypes.string,
   companyLogo: PropTypes.object,
+  generalLogo: PropTypes.object,
   rating: PropTypes.string,
-  text: PropTypes.string,
-  // remove -->
   stars: PropTypes.object,
-  clutch: PropTypes.object,
+  text: PropTypes.string,
 };
 
 export default Item;
