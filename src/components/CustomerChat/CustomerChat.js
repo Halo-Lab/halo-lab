2b2ee603469c9726d4284cfb0ed7babc1cc8f098
mikhailnetkachev-halolab <michael.n@halo-lab.com>
2020-02-28 10:16:52 +0200

    Edited config.

--- src/components/CustomerChat/CustomerChat.js
+++ src/components/CustomerChat/CustomerChat.js
@@ -2,11 +2,9 @@ import React from 'react';
 import MessengerCustomerChat from 'react-messenger-customer-chat';
 
 const config = {
-  autoLogAppEvents: true,
   greetingDialogDisplay: 'hide',
   themeColor: '#3719CA',
   version: '3.3',
-  xfbml: true,
 };
 
 const CustomerChat = () => {
