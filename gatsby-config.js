a6ae14cff07c36018594eec42ad7f474f41d6c09
mikhailnetkachev-halolab <michael.n@halo-lab.com>
2020-03-02 12:50:45 +0200

    Moved images and fonts to new folder.

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,14 +22,14 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `${__dirname}/src/images`,
+        path: `${__dirname}/src/assets/images`,
       },
     },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `fonts`,
-        path: `${__dirname}/src/fonts/`,
+        path: `${__dirname}/src/assets/fonts/`,
       },
     },
     {
