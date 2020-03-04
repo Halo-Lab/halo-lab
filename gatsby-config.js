7bd26803a7bfdeb4e90667f1f8782df0696d9aa1
mikhailnetkachev-halolab <michael.n@halo-lab.com>
2020-03-04 14:10:52 +0200

    All pages will be cached.

--- gatsby-config.js
+++ gatsby-config.js
@@ -54,7 +54,7 @@ module.exports = {
           `/portfolio/`,
           `/services/`,
           `/blog/`,
-          // `/blog/*`,
+          `/blog/*`,
           `/contacts/`,
           `/thanks/`,
           `/404/`,
