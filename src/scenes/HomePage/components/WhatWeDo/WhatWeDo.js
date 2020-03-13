87778cbc0fc573fcbfa6830b9d99650e1bd09125
Александр Творогов <tvorogov100500@gmail.com>
2020-03-13 17:05:28 +0200

    update what we do section

--- src/scenes/HomePage/components/WhatWeDo/WhatWeDo.js
+++ src/scenes/HomePage/components/WhatWeDo/WhatWeDo.js
@@ -5,19 +5,17 @@ import styles from './WhatWeDo.module.scss';
 const WhatWeDo = () => {
   return (
     <section className={styles.container}>
-      <div className={styles.inner}>
-        <div className={styles.title}>
-          Driving your business forward with strong products
+      <div className={styles.title}>
+        Driving your business forward with strong products
         </div>
 
-        <div className={styles.description}>
-          We are a full-service digital agency that builds immersive user
-          experience. Our team creates an exceptional visualization and
-          thought-out functionality. We believe, our clients deserve to be
-          remarkable in their business. The studio develops the products people
-          appreciate all around the world.
+      <div className={styles.description}>
+        We are a full-service digital agency that builds immersive user
+        experience. Our team creates an exceptional visualization and
+        thought-out functionality. We believe, our clients deserve to be
+        remarkable in their business. The studio develops the products people
+        appreciate all around the world.
         </div>
-      </div>
     </section>
   );
 };
