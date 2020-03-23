0b8f124cf7db0e40294130ddb3622bdc4dc820ea
Александр Творогов <tvorogov100500@gmail.com>
2020-03-23 11:16:59 +0200

    remove commets from footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,5 @@
 import React from 'react';
 
-// import { useFooterAssets } from '@hooks/queries';
-
 import styles from './Footer.module.scss';
 import Twitter from '../../assets/images/brands/twitter.inline.svg';
 import Dribbble from '../../assets/images/brands/dribbble.inline.svg';
@@ -10,30 +8,6 @@ import Instagram from '../../assets/images/brands/instagram.inline.svg';
 import Behance from '../../assets/images/brands/behance.inline.svg';
 
 const Footer = () => {
-  // const { behance, dribbble, facebook, instagram, twitter } = useFooterAssets();
-  // const socials = [
-  //   {
-  //     href: 'https://twitter.com/halolabteam',
-  //     image: twitter,
-  //   },
-  //   {
-  //     href: 'https://www.instagram.com/halolabteam/',
-  //     image: instagram,
-  //   },
-  //   {
-  //     href: 'https://www.facebook.com/halolabteam/',
-  //     image: facebook,
-  //   },
-  //   {
-  //     href: 'https://www.behance.net/halolab',
-  //     image: behance,
-  //   },
-  //   {
-  //     href: 'https://dribbble.com/halolab',
-  //     image: dribbble,
-  //   },
-  // ];
-
   return (
     <div className={`pageWrapper ${styles.container}`}>
       <div className={styles.logotype}>
@@ -85,16 +59,6 @@ const Footer = () => {
             <Dribbble />
           </a>
         </li>
-        {/* {socials.map(({ href, image }) => {
-          return (
-            <li key={href}>
-              <a href={href} target="_blank" rel="noopener noreferrer">
-                {image}
-                <img src={image.publicURL} alt="social icon" loading="lazy" />
-              </a>
-            </li>
-          );
-        })} */}
       </ul>
     </div>
   );
