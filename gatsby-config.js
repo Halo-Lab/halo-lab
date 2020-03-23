2fc40f67f1ce4c62542b569ff6f363b4cbe2afc2
Александр Творогов <tvorogov100500@gmail.com>
2020-03-23 10:29:12 +0200

    fix desing, change footer, resolve commetns

--- gatsby-config.js
+++ gatsby-config.js
@@ -134,5 +134,13 @@ module.exports = {
         },
       },
     },
+    {
+      resolve: 'gatsby-plugin-react-svg',
+      options: {
+        rule: {
+          include: /\.inline\.svg$/,
+        },
+      },
+    },
   ],
 };
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,41 +1,38 @@
 import React from 'react';
 
-import { useFooterAssets } from '@hooks/queries';
+// import { useFooterAssets } from '@hooks/queries';
 
 import styles from './Footer.module.scss';
+import Twitter from '../../assets/images/brands/twitter.inline.svg';
+import Dribbble from '../../assets/images/brands/dribbble.inline.svg';
+import Facebook from '../../assets/images/brands/facebook.inline.svg';
+import Instagram from '../../assets/images/brands/instagram.inline.svg';
+import Behance from '../../assets/images/brands/behance.inline.svg';
 
 const Footer = () => {
-  const {
-    // logotype,
-    behance,
-    dribbble,
-    facebook,
-    instagram,
-    twitter,
-  } = useFooterAssets();
-
-  const socials = [
-    {
-      href: 'https://twitter.com/halolabteam',
-      image: twitter,
-    },
-    {
-      href: 'https://www.instagram.com/halolabteam/',
-      image: instagram,
-    },
-    {
-      href: 'https://www.facebook.com/halolabteam/',
-      image: facebook,
-    },
-    {
-      href: 'https://www.behance.net/halolab',
-      image: behance,
-    },
-    {
-      href: 'https://dribbble.com/halolab',
-      image: dribbble,
-    },
-  ];
+  // const { behance, dribbble, facebook, instagram, twitter } = useFooterAssets();
+  // const socials = [
+  //   {
+  //     href: 'https://twitter.com/halolabteam',
+  //     image: twitter,
+  //   },
+  //   {
+  //     href: 'https://www.instagram.com/halolabteam/',
+  //     image: instagram,
+  //   },
+  //   {
+  //     href: 'https://www.facebook.com/halolabteam/',
+  //     image: facebook,
+  //   },
+  //   {
+  //     href: 'https://www.behance.net/halolab',
+  //     image: behance,
+  //   },
+  //   {
+  //     href: 'https://dribbble.com/halolab',
+  //     image: dribbble,
+  //   },
+  // ];
 
   return (
     <div className={`pageWrapper ${styles.container}`}>
@@ -43,15 +40,61 @@ const Footer = () => {
         With Love from <b>Halo Lab</b>
       </div>
       <ul className={styles.socials}>
-        {socials.map(({ href, image }) => {
+        <li>
+          <a
+            href="https://twitter.com/halolabteam"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Twitter />
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.instagram.com/halolabteam/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Instagram />
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.facebook.com/halolabteam/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Facebook />
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.behance.net/halolab"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Behance />
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://dribbble.com/halolab"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Dribbble />
+          </a>
+        </li>
+        {/* {socials.map(({ href, image }) => {
           return (
             <li key={href}>
               <a href={href} target="_blank" rel="noopener noreferrer">
+                {image}
                 <img src={image.publicURL} alt="social icon" loading="lazy" />
               </a>
             </li>
           );
-        })}
+        })} */}
       </ul>
     </div>
   );
