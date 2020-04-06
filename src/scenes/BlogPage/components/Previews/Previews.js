532fb918312ae45b66056edbf0ffff40fcb42e19
Александр Творогов <tvorogov100500@gmail.com>
2020-04-06 14:50:13 +0300

    remove comment

--- src/scenes/BlogPage/components/Previews/Previews.js
+++ src/scenes/BlogPage/components/Previews/Previews.js
@@ -1,21 +1,14 @@
 import React, { Fragment } from 'react';
 
-// import List from './components/List';
 import Switcher from './components/Switcher';
-// import { useBreakpoints, BREAKPOINTS } from '@hooks';
 import { useWordpressPosts } from '@hooks/queries';
 
 const Previews = () => {
-  // const { breakpoint } = useBreakpoints();
   const items = useWordpressPosts();
 
   return (
     <Fragment>
       <Switcher items={items} />
-      {/* {breakpoint !== BREAKPOINTS.MOBILE ? (
-      ) : (
-        <List items={items} />
-      )} */}
     </Fragment>
   );
 };
