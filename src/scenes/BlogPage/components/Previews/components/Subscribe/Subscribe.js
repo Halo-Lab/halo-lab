2f6eea08817f76e58ca0f83f2aec2f050a0000a7
Александр Творогов <tvorogov100500@gmail.com>
2020-04-06 11:34:30 +0300

    add validation to subscrive

--- src/scenes/BlogPage/components/Previews/components/Subscribe/Subscribe.js
+++ src/scenes/BlogPage/components/Previews/components/Subscribe/Subscribe.js
@@ -1,8 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import styles from './Subscribe.module.scss';
 
 const Subscribe = () => {
+  const [data, setData] = useState({
+    email: {
+      value: '',
+      valid: false,
+    },
+  });
+  const [isValid, setIsValid] = useState(true);
+
+  const isCompanyEmailValid = email => {
+    const regexp = new RegExp(/[^@]+@[^.]+\..+/g);
+    return regexp.test(email);
+  };
+
+  const handleChange = ({ target: { name, value } }) => {
+    console.log(value);
+    setData(data => ({
+      ...data,
+      [name]: {
+        valid: name === 'email' ? isCompanyEmailValid(value) : value,
+        value: value,
+      },
+    }));
+  };
+
   return (
     <div className={styles.container}>
       <div className={styles.block}>
@@ -13,11 +37,14 @@ const Subscribe = () => {
               Type your email
             </label>
             <input
-              className={styles.input}
+              className={`${styles.input} ${
+                !data.email.valid ? styles.error : ''
+              }`}
               type="text"
               name="email"
               id="email"
               require="true"
+              onChange={handleChange}
             />
           </div>
         </form>
