c2982327dd3f71ba4f8837065db03c98a81900ba
lazio <diniovskiy@gmail.com>
2020-06-03 18:30:45 +0300

    fix eslint

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,5 @@
 import React, { useContext } from 'react';
 import { Link } from 'gatsby';
-import PropTypes from 'prop-types';
 import classNames from 'classnames';
 
 import { MenuContext } from '@contexts';
