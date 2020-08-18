2830030ac4b1af6dbe025a83fb66fa604dad7101
aleksandr-tyagun <alexander.tiagun@gmail.com>
2020-08-18 17:57:44 +0300

    webpack-bundle-analyzer plugin was added

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,6 +4,28 @@
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
 
+const packageJsonFile = require('./package.json');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+
+exports.onCreateWebpackConfig = ({ stage, actions }) => {
+  if (stage === 'build-javascript') {
+    const defaultOptions = {
+      statsFilename: `../reports/stats_v${packageJsonFile.version}.json`,
+      reportFilename: `../reports/report_v${packageJsonFile.version}.html`,
+      openAnalyzer: false,
+      analyzerMode: 'static',
+      generateStatsFile: true,
+      statsOptions: {
+        source: false,
+      },
+    };
+
+    actions.setWebpackConfig({
+      plugins: [new BundleAnalyzerPlugin(defaultOptions)],
+    });
+  }
+};
+
 exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions;
 
