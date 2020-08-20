c199bb20115bcc6ff8f9eedd75cae11bb43403f0
Alexey Rybalko <magurawork@gmail.com>
2020-08-20 15:55:22 +0300

    test without blocks

--- src/scenes/HomePage/HomePage.js
+++ src/scenes/HomePage/HomePage.js
@@ -1,18 +1,11 @@
 import React from 'react';
-import Projects from '@scenes/Projects';
-import MailUs from '@scenes/MailUs';
-import Hero from './components/Hero';
-import WhatWeDo from './components/WhatWeDo';
-import Testimonials from './components/Testimonials';
 import Gallery from './components/Gallery';
-import Posts from './components/Posts';
-import Works from './components/Works';
 
 import PropTypes from 'prop-types';
 
 import styles from './HomePage.module.scss';
 
-const HomePage = ({ animation }) => {
+const HomePage = () => {
   return (
     <div className={styles.container}>
       <Gallery />
