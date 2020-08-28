1a79785bcc429ef729467a7e188f82c0bc4ed8a1
olga_tim <tigercub220@gmail.com>
2020-08-28 12:39:11 +0300

    Change image to gif

--- src/scenes/NotFoundPage/components/Title/Title.js
+++ src/scenes/NotFoundPage/components/Title/Title.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'gatsby';
-import image from './images/man-image.png';
+import image from './images/man-image.gif';
 
 import styles from './Title.module.scss';
 
@@ -8,10 +8,15 @@ const Title = () => {
   return (
     <div className={styles.container}>
       <div className={styles.message}>
-        <p className={styles.text}>
-          Gosh, it seems like page you’re looking for has been absorbed.
-          <img src={image} className={styles.image} alt="crying man" />
-        </p>
+        <div className={styles.textWrap}>
+          <p className={styles.text}>
+            Gosh, it seems like page you’re looking for has been absorbed.
+          </p>
+          <div className={styles.imageWrap}>
+            <img src={image} className={styles.image} alt="crying man" />
+          </div>
+        </div>
+
         <Link to="/" className={styles.link}>
           okay, go on 😢
         </Link>
