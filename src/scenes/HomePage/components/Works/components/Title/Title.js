40256c6fc946055c0e7ce1bbbbfd8ea0e47f614d
admin <admin@iMac-Konstantin.local>
2020-09-22 10:38:23 +0300

    change color with transition

--- src/scenes/HomePage/components/Works/components/Title/Title.js
+++ src/scenes/HomePage/components/Works/components/Title/Title.js
@@ -10,7 +10,7 @@ const Title = ({ icon, signature }) => {
       transform: `rotate(${window.pageYOffset / 3}deg)`,
     });
   };
-
+  console.log(styles.svg)
   useEffect(() => {
     window.addEventListener('scroll', handleScroll);
     return function remove() {
@@ -37,8 +37,8 @@ const Title = ({ icon, signature }) => {
           src={icon.publicURL}
           alt="dribbble logotype"
           loading="lazy"
-          className={styles.image}
-          style={dribbleTextTransform}
+          className={styles.svg}
+          // style={dribbleTextTransform}
         />
       </a>
     </div>
