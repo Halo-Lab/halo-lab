243720230e52ddfc4b4c96caac4207f636e18744
lazio <diniovskiy@gmail.com>
2021-03-26 16:40:36 +0200

    uncomment test

--- cypress/integration/homepage.spec.js
+++ cypress/integration/homepage.spec.js
@@ -3,13 +3,13 @@ describe('homepage', function() {
     cy.visit(`/`);
   });
 
-  // it('test more posts link in blog section', function() {
-  //   cy.contains(
-  //     'Front-end & back-end expertise from development to delivery'
-  //   ).scrollIntoView();
-  //   cy.contains('MORE BLOG POSTS').click();
-  //   cy.contains('Our Blog').should('be.visible');
-  // });
+  it('test more posts link in blog section', function() {
+    cy.contains(
+      'Front-end & back-end expertise from development to delivery'
+    ).scrollIntoView();
+    cy.contains('MORE BLOG POSTS').click();
+    cy.contains('Our Blog').should('be.visible');
+  });
 
   it('test navigation to the latest blog post from homepage', function() {
     cy.contains(
