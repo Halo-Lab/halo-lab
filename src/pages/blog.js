143d14de272ae5bb3f4fcfcc9a13c8a48af10974
pastukhVitalii <pastukh.v.7@gmail.com>
2021-04-21 17:24:55 +0300

    [Blog] Pagination for blog categories should be generated from gatsby-node file

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -5,20 +5,23 @@ import Layout from '@components/Layout';
 import Head from '@components/Head';
 import BackgroundStars from '@components/BackgroundStars';
 import BlogPage from '@scenes/BlogPage';
+import PropTypes from "prop-types";
 
-const Blog = () => {
+const Blog = ({ pageContext }) => {
   const title = 'Blog - Halo Lab';
   const description = `If you would like to find some useful articles about digital products - welcome to our blog page. Here we tell you about design and development, we share our case studies with you, and we talk about logos and branding.`;
-
+  const { page } = pageContext;
   return (
     <Providers>
       <BackgroundStars />
       <Layout>
         <Head title={title} description={description}></Head>
-        <BlogPage />
+        <BlogPage page={page}/>
       </Layout>
     </Providers>
   );
 };
-
+Blog.propTypes = {
+  pageContext: PropTypes.object,
+};
 export default Blog;
--- src/scenes/BlogPage/components/Previews/Previews.js
+++ src/scenes/BlogPage/components/Previews/Previews.js
@@ -1,16 +1,19 @@
-import React, { Fragment } from 'react';
+import React, { Fragment } from "react";
 
-import Switcher from './components/Switcher';
-import { useWordpressPosts } from '@hooks/queries';
+import Switcher from "./components/Switcher";
+import { useWordpressPosts } from "@hooks/queries";
+import PropTypes from "prop-types";
 
-const Previews = () => {
+const Previews = (props) => {
   const items = useWordpressPosts();
 
   return (
     <Fragment>
-      <Switcher items={items} />
+      <Switcher items={items} page={props.page}/>
     </Fragment>
   );
 };
-
+Previews.propTypes = {
+  page: PropTypes.number,
+};
 export default Previews;
